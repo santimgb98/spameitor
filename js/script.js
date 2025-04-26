8daf418e7f3c661465c62c9bb99fac5ca25445e4
santimgb98 <santimgb98@gmail.com>
2025-04-26 11:31:02 +0200

    Some styles and functions were added to the jQuery script, and the graphical interface was also improved.

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ $(document).ready(function(){
     $("#btn_send_spam").click(function(){
         $(".font").hide()
         $(".input").hide()
+        $(".input_message").hide()
         $("#btn_send_spam").hide()
         $("#aparece").show();
         $("#btn_return").show();
@@ -25,9 +26,17 @@ $(document).ready(function(){
     $("#btn_return").click(function(){
         $(".font").show()
         $(".input").show()
+        $(".input_message").show()
         $("#btn_send_spam").show()
         $("#aparece").hide();
         $("#btn_return").hide();
+    })
+
+    $("#btn_send_spam").mouseenter(function(){
+        $(this).text("👻 Send spam 👻")
+    })
+    $("#btn_send_spam").mouseleave(function(){
+        $(this).text("Send spam")
     })
     
    
